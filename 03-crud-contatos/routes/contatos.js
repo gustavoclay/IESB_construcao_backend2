adf48234cc344f153743b327001889e462f08c8b
gustavoclay <gustavoclay@gmail.com>
2024-04-05 22:00:29 -0300

    Finalizando crud simples de contatos

--- 03-crud-contatos/routes/contatos.js
+++ 03-crud-contatos/routes/contatos.js
@@ -9,7 +9,32 @@ router.get('/contatos', (req, res) => {
     res.json(listaContatos)
 })
 
-// 
+// READ -> Busca de contato por id
+router.get('/contatos/:id', (req, res) => {
+    const id = req.params.id
+    res.json(listaContatos[id])
+})
 
+// CREATE -> Criar um contato
+router.post('/contatos', (req, res) => {
+    const contato = req.body
+    listaContatos.push(contato.nome)
+    res.json({ mensagem: "Contato criado com sucesso!" })
+})
+
+// DELETE -> Deletar um contato
+router.delete('/contatos/:id', (req, res) => {
+    const id = req.params.id
+    listaContatos.splice(id, 1)
+    res.json({ mensagem: "Contato excluido com sucesso!" })
+})
+
+// UPDATE -> Atualizar um contato
+router.put('/contatos/:id', (req, res) => {
+    const id = req.params.id
+    const contatoAlterado = req.body
+    listaContatos[id] = contatoAlterado.nome
+    res.json({ mensagem: "Contato alterado com sucesso!" })
+})
 
 module.exports = router
